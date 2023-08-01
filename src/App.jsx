222400446f12915c6b1d65fd840c47bf171255ac
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-01 13:37:56 -0300

    Creación elemento header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className="container-fluid">
+        <div className="row">
+          <div className="col text-center">
+            <h3 id="my-tinerary">My Tinerary</h3>
+          </div>
+          <div className="col text-center">
+            <nav>
+              <a href="./index.html" id="home" className="me-4 text-black">
+                Home
+              </a>
+              <a href="./cities.html" id="cities" className="me-4 text-black">
+                Cities
+              </a>
+              <button
+                type="button"
+                id="login"
+                class="btn btn-primary px-4 mb-1"
+              >
+                Login
+              </button>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </main>
       <footer></footer>
     </>
   );
