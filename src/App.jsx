b879c926f3b3f9cf2bc838b0b8fc6aef5e65f60b
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-02 00:00:21 -0300

    Estructura armada

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ function App() {
   return (
     <>
       <header className="container-fluid">
-        <div className="row">
-          <div className="col text-center">
+        <div className="row justify-content-center">
+          <div className="col-4 text-start">
             <h3 id="my-tinerary">My Tinerary</h3>
           </div>
-          <div className="col text-center">
+          <div className="col-4 text-end">
             <nav>
               <a href="./index.html" id="home" className="me-4 text-black">
                 Home
@@ -27,10 +27,24 @@ function App() {
           </div>
         </div>
       </header>
-      <main>
-        <div>
-          <div></div>
-          <div></div>
+      <main className="container-fluid">
+        <div className="row justify-content-center mainClass">
+          <div className="col-4 text-start ">
+            <h3 className="col mb-4" id="find">
+              Find the perfect destination
+            </h3>
+            <h6 className="col mb-4 text-start" id="our">
+              Our app will help you find the perfect path for your next trip.
+              With an easy-to-use interface and a host of itinerary options,
+              planning your next trip has never been easier.
+            </h6>
+            <button type="button" id="view" class="btn btn-primary px-5 mb-1">
+              Viem More
+            </button>
+          </div>
+          <div className="col-4 text-start">
+            <img src="" alt="" />
+          </div>
         </div>
       </main>
       <footer></footer>
