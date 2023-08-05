3a128d09957e10977f48a9400491f157497f88ff
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-05 17:40:21 -0300

    Estructura componente Urls por fila

--- src/components/Urls/index.jsx
+++ src/components/Urls/index.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-export default function Urls({ row }) {
+export default function Urls({
+  row,
+  imgContainer,
+  textContainer,
+  mainTitle,
+  subTitle,
+  mainName,
+  subName,
+}) {
   return (
     <>
-      <div className={row}></div>
+      <div className={row}>
+        <div className={imgContainer}>
+          <div className={textContainer}>
+            <p className={mainTitle}>{mainName}</p>
+            <p className={subTitle}>{subName}</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
