472003b0116682637bcf2b58b5146290fa5ddbdd
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-06 14:56:44 -0300

    Acomodando componentes

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./style.css";
+import Carousel from "../Carousel";
 
 function Main() {
   return (
-    <main class="container-fluid">
-      <div class="row justify-content-center mainClass">
+    <main class="">
+      <div class="justify-content-center d-flex align-items-center mainClass">
         <div class="col-4 text-start ">
           <h3 class="col mb-4" id="find">
             Find the perfect destination
@@ -22,128 +23,7 @@ function Main() {
           <img src="src\assets\rectangule.png" alt="fill-img" />
         </div>
       </div>
-      <div
-        id="carouselDiv"
-        class="row carousel slide content-center "
-        data-bs-ride="carousel"
-      >
-        <div class="container text-center divPopular">
-          <p>Popular Mytineraries</p>
-        </div>
-        <div class="carousel-inner imgDiv">
-          <div
-            class="carousel-item active container-fluid"
-            data-bs-interval="10000"
-          >
-            <div class="d-flex justify-content-center">
-              <div class="m-1 img1">
-                <div class="textcontainer">
-                  <p class="imgp">Tokyo</p>
-                  <p class="imgsub">Japan</p>
-                </div>
-              </div>
-              <div class="m-1 img2">
-                <div class="textcontainer">
-                  <p class="imgp">New york</p>
-                  <p class="imgsub">Usa</p>
-                </div>
-              </div>
-            </div>
-            <div class="d-flex justify-content-center">
-              <div class="m-1 img3">
-                <div class="textcontainer">
-                  <p class="imgp">París</p>
-                  <p class="imgsub">France</p>
-                </div>
-              </div>
-              <div class="m-1 img4">
-                <div class="textcontainer">
-                  <p class="imgp">Seoul</p>
-                  <p class="imgsub">South Korea</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item" data-bs-interval="2000">
-            <div class="d-flex justify-content-center">
-              <div class="m-1 img1 row">
-                <div class="textcontainer">
-                  <p class="imgp">Tokyo</p>
-                  <p class="imgsub">Japan</p>
-                </div>
-              </div>
-              <div class="m-1 img2">
-                <div class="textcontainer">
-                  <p class="imgp">New york</p>
-                  <p class="imgsub">Usa</p>
-                </div>
-              </div>
-            </div>
-            <div class="d-flex justify-content-center">
-              <div class="m-1 img3">
-                <div class="textcontainer">
-                  <p class="imgp">París</p>
-                  <p class="imgsub">France</p>
-                </div>
-              </div>
-              <div class="m-1 img4">
-                <div class="textcontainer">
-                  <p class="imgp">Seoul</p>
-                  <p class="imgsub">South Korea</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="d-flex justify-content-center">
-              <div class="m-1 img1 row">
-                <div class="textcontainer">
-                  <p class="imgp">Tokyo</p>
-                  <p class="imgsub">Japan</p>
-                </div>
-              </div>
-              <div class="m-1 img2">
-                <div class="textcontainer">
-                  <p class="imgp">New york</p>
-                  <p class="imgsub">Usa</p>
-                </div>
-              </div>
-            </div>
-            <div class="d-flex justify-content-center">
-              <div class="m-1 img3">
-                <div class="textcontainer">
-                  <p class="imgp">París</p>
-                  <p class="imgsub">France</p>
-                </div>
-              </div>
-              <div class="m-1 img4">
-                <div class="textcontainer">
-                  <p class="imgp">Seoul</p>
-                  <p class="imgsub">South Korea</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselDiv"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselDiv"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
+      <Carousel />
     </main>
   );
 }
