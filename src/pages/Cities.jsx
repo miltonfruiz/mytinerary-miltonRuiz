51c7eac10dd937b4f2613f09ae3d7d06e5ef0397
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-08 13:57:58 -0300

    Creación archivo Cities.jsx

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import Main from "../components/Main";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function Home() {
+export default function Cities() {
   return (
     <>
       <Header />
-      <Main />
+      <p>TEXTO</p>
       <Footer />
     </>
   );
