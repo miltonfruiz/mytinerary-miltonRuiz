97a57351e32537dfed7a77de26603ffd83c9e0f1
Milton Franco Ruiz <miltonfruizok@outlook.es>
2023-08-10 12:18:02 -0300

    Agregado de sombras en icono

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -48,7 +48,7 @@ export default function Carousel() {
       )}
       <FaChevronCircleLeft
         size={25}
-        className="prew"
+        className="prew "
         onClick={() => handlerPrew()}
       />
       <FaChevronCircleRight
